2bfabc8d6582bd9d1ef929038c45376a2505c45e
camhammond <chammond@myfairpoint.net>
2020-02-22 21:37:16 -0500

    Updating OI

--- 2020InfiniteRecharge/src/main/include/OI.h
+++ 2020InfiniteRecharge/src/main/include/OI.h
@@ -39,26 +39,16 @@ class OI {
   // Setup driver controller
   frc::Joystick *driverController = new frc::Joystick(DRIVER_CONTROLLER);
   // Setup buttons on the driver controller, change the name to fit the command
-  frc::JoystickButton *driverController_button_a =
-      new frc::JoystickButton(driverController, BUTTON_A); // Manual Elevator
-  frc::JoystickButton *driverController_button_b =
-      new frc::JoystickButton(driverController, BUTTON_B); // Not Used
-  frc::JoystickButton *driverController_button_x =
-      new frc::JoystickButton(driverController, BUTTON_X); // Not Used
-  frc::JoystickButton *driverController_button_y =
-      new frc::JoystickButton(driverController, BUTTON_Y); // Manual Elevator
-  frc::JoystickButton *driverController_button_lbump =
-      new frc::JoystickButton(driverController, BUTTON_L_BUMP); // Not Used
-  frc::JoystickButton *driverController_button_rbump = new frc::JoystickButton(
-      driverController, BUTTON_R_BUMP); // Used in gear shifting
-  frc::JoystickButton *driverController_button_select =
-      new frc::JoystickButton(driverController, BUTTON_SELECT); // Lineup button
-  frc::JoystickButton *driverController_button_start =
-      new frc::JoystickButton(driverController, BUTTON_START); // Not Used
-  frc::JoystickButton *driverController_button_l3 =
-      new frc::JoystickButton(driverController, BUTTON_L3); // Not Used
-  frc::JoystickButton *driverController_button_r3 =
-      new frc::JoystickButton(driverController, BUTTON_R3); // Not Used
+  frc::JoystickButton *driverController_button_a = new frc::JoystickButton(driverController, BUTTON_A); // Spinup wheels and aquire target from limelight
+  frc::JoystickButton *driverController_button_b = new frc::JoystickButton(driverController, BUTTON_B); // Low gear mode
+  frc::JoystickButton *driverController_button_x = new frc::JoystickButton(driverController, BUTTON_X); // Shoot the ball
+  frc::JoystickButton *driverController_button_y = new frc::JoystickButton(driverController, BUTTON_Y); // Intake ball
+  frc::JoystickButton *driverController_button_lbump = new frc::JoystickButton(driverController, BUTTON_L_BUMP); // Not Used
+  frc::JoystickButton *driverController_button_rbump = new frc::JoystickButton(driverController, BUTTON_R_BUMP); // Used in gear shifting
+  frc::JoystickButton *driverController_button_select = new frc::JoystickButton(driverController, BUTTON_SELECT); // Toggle Shooter between manual/auto mode 
+  frc::JoystickButton *driverController_button_start = new frc::JoystickButton(driverController, BUTTON_START); // Not Used
+  frc::JoystickButton *driverController_button_l3 = new frc::JoystickButton(driverController, BUTTON_L3); // Not Used
+  frc::JoystickButton *driverController_button_r3 = new frc::JoystickButton(driverController, BUTTON_R3); // Not Used
   // LEFT TRIGGER // Drive Backwards
   // RIGHT TRIGGER // Drive Forwards
   // LEFT STICK LEFT // Turn Left
@@ -67,7 +57,7 @@ class OI {
   // Setup aux controller
   frc::Joystick *auxController = new frc::Joystick(AUX_CONTROLLER);
   // Setup buttons on the aux controller, change the names to fit the command
-  frc::JoystickButton *auxController_button_a = new frc::JoystickButton(auxController, BUTTON_A); 
+  frc::JoystickButton *auxController_button_a = new frc::JoystickButton(auxController, BUTTON_A); // 
   frc::JoystickButton *auxController_button_b = new frc::JoystickButton(auxController, BUTTON_B); 
   frc::JoystickButton *auxController_button_x = new frc::JoystickButton(auxController, BUTTON_X); 
   frc::JoystickButton *auxController_button_y = new frc::JoystickButton(auxController, BUTTON_Y); 
